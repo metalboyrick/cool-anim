a4c14f36588e983c3fd476f766b83234f473cb51
metalboyrick <rsulisthio@gmail.com>
2023-06-16 21:51:51 +0700

    feat: add color configuration

---
+++ src/config/theme.ts
@@ -0,0 +1,12 @@
+export const colors = {
+  blue: {
+    medium: "#4193FE",
+  },
+  gray: {
+    light: "#C7C0DF80",
+    medium: "#3C3949",
+    dark: "#201D2C",
+  },
+  black: "##111014",
+  white: "#FFFFFF",
+};
